8b46790f1e6ef4a805a04dcd9d353a7ca8a483a1
oliverbravery <dev@oliverbravery.uk>
2023-04-15 21:11:20 +0100

    Initial Commit

---
+++ ResizableViews/ResizableViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResizableViewsApp.swift
+//  ResizableViews
+//
+//  Created by Oliver Bravery on 15/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct ResizableViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
